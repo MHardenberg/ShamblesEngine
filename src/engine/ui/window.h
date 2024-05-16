463a970a87bfffd62c1c04fdb3cf0eeee5af4f4f
Michel Hardenberg <MHardenberg@proton.me>
2024-05-16 19:44:55 +0200

    up

---
+++ src/engine/ui/window.h
@@ -0,0 +1,14 @@
+#ifndef window_H
+#define window_H
+
+struct SHAM_window {
+    
+};
+
+
+
+
+
+
+
+#endif // !window_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+//#include <shamCore.h>
+#include "core/shamCore.h"
+#include "engine/ui/window.h"
 
 int main() {
+    i64 x = 5;
     printf("We're in shambles!!!!!\n");
     return 0;
 }
